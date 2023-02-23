5a696cdd929164ea53b1f6b8a7f610f783d54695
Nguyen Thanh Dat <109453487+DatSung@users.noreply.github.com>
2023-02-23 20:51:00 +0700

    Initial commit

---
+++ CraftsMan/src/CraftsMan/CraftsMan.java
@@ -0,0 +1,10 @@
+package CraftsMan;
+
+import data.Rectangle;
+
+public class CraftsMan {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle("TIA", "PINK", 1.0,2.0);
+        r1.paint();
+    }
+}
